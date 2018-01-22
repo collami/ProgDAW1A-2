9060089ba531a78d70f2bb7d94d2dac7009e3880
collami <32308543+collami@users.noreply.github.com>
2018-01-22 23:09:09 +0100

    Update Ejercicio2.java

--- Práctica 2/Ejercicio2.java
+++ Práctica 2/Ejercicio2.java
@@ -18,27 +18,18 @@ public class Ejercicio2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[][] matriz1;
-        int[][] matriz2;
-        matriz1 = new int[4][4];
-        matriz2 = new int[4][4];
+        int[][] matriz;
+        matriz = new int[4][4];
         int contador =0;
         Scanner teclado = new Scanner(System.in);
-        for (int i = 0; i < matriz1.length; i++) {
-            for (int j = 0; j < matriz1[i].length; j++) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
                 System.out.print("Ingrese un número para la primera matriz bidimensional, para la posición "+i+","+j+": ");
-                matriz1[i][j]=teclado.nextInt();
-            }
-        }    
-        for (int i = 0; i < matriz2.length; i++) {
-            for (int j = 0; j < matriz2[i].length; j++) {
-                System.out.print("Ingrese un número para la segunda matriz bidimensional, para la posición "+i+","+j+": ");
-                matriz2[i][j]=teclado.nextInt();
-            }    
-        }          
-            for (int i = 0; i < matriz1.length; i++) {
-                for (int j = 0; j < matriz1[i].length; j++) {
-                    if (matriz1[i][j]!= matriz2[i][j]){
+                matriz[i][j]=teclado.nextInt();
+            }   
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    if (matriz[i][j]!= matriz[j][i]){
                         contador=1;
                     }
                 }
