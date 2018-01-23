969f3d77195b861876110ccc5fc908466fa8df8a
collami <32308543+collami@users.noreply.github.com>
2018-01-23 17:37:56 +0100

    Add files via upload

--- Práctica 2/Ejercicio2.java
+++ Práctica 2/Ejercicio2.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * @author infor14
  */
 public class Ejercicio2 {
-   
+
     /**
      * @param args the command line arguments
      */
@@ -20,25 +20,26 @@ public class Ejercicio2 {
         // TODO code application logic here
         int[][] matriz;
         matriz = new int[4][4];
-        int contador =0;
+        int contador = 0;
         Scanner teclado = new Scanner(System.in);
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print("Ingrese un número para la primera matriz bidimensional, para la posición "+i+","+j+": ");
-                matriz[i][j]=teclado.nextInt();
-            }   
-            for (int i = 0; i < matriz.length; i++) {
-                for (int j = 0; j < matriz[i].length; j++) {
-                    if (matriz[i][j]!= matriz[j][i]){
-                        contador=1;
-                    }
+                System.out.print("Ingrese un número para la primera matriz bidimensional, para la posición " + i + "," + j + ": ");
+                matriz[i][j] = teclado.nextInt();
+            }
+        }
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] != matriz[j][i]) {
+                    contador = 1;
                 }
             }
-        if (contador==0) {
-            System.out.println("La matriz bidimensional es simétrica");
         }
-        else {
+        if (contador == 0) {
+            System.out.println("La matriz bidimensional es simétrica");
+        } else {
             System.out.println("La matriz bidimensional no es simétrica");
         }
-    }   
+    }
 }
